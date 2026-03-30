344589a21fab4212c26e579d9b7d1d487dc136ea
Emmanuel Colon <ecolon714@gmail.com>
2026-03-30 14:46:46 -0400

    Initial Selenium test framework setup

---
+++ tests/test_login.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from pages.login_page import LoginPage
+from pages.inventory_page import InventoryPage
+
+
+def test_valid_login():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    login_page = LoginPage(driver)
+    login_page.load()
+    login_page.login("standard_user", "secret_sauce")
+
+    assert "inventory" in driver.current_url 
+
+    driver.quit()
+
+
+def test_add_to_cart():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    login_page = LoginPage(driver)
+    login_page.load()
+    login_page.login("standard_user", "secret_sauce")
+
+    inventory = InventoryPage(driver)
+    inventory.add_first_item_to_cart()
+    inventory.go_to_cart()
+
+    assert "cart" in driver.current_url
+
+    driver.quit()
