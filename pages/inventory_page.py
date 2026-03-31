269b559c6a1317de5bf9dcb13e6b648f1b827394
Emmanuel Colon <ecolon714@gmail.com>
2026-03-31 11:30:29 -0400

    Added explicit waits to improve test stability

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,11 +1,18 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC 
 
 class InventoryPage:
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
 
     def add_first_item_to_cart(self):
-        self.driver.find_element(By.CLASS_NAME, "btn_inventory").click()  
+        self.wait.until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "btn_inventory"))
+        ).click()
 
     def go_to_cart(self):
-        self.driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
+        self.wait.until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "shopping_cart_link"))
+        ).click()
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,16 +1,28 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC 
 from config.settings import BASE_URL
 
 
 class LoginPage:
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
 
     def load(self):
         self.driver.get(BASE_URL)
 
     def login(self, username, password):
-        self.driver.find_element(By.ID, "user-name").send_keys(username)
-        self.driver.find_element(By.ID, "password").send_keys(password)
-        self.driver.find_element(By.ID, "login-button").click()
+
+        self.wait.until(
+            EC.visibility_of_element_located((By.ID, "user-name"))
+        ).send_keys(username)
+
+        self.wait.until(
+            EC.visibility_of_element_located((By.ID, "password"))
+        ).send_keys(password)
+
+        self.wait.until(
+            EC.element_to_be_clickable((By.ID, "login-button"))
+        ).click()
         
